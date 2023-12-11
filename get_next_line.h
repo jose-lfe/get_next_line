2a3c8b8203689ecdac724ffa89773cdd83c4d0bd
Jose Luis Fernandes Pereira <jose-lfe@c1r3s7.42lausanne.ch>
2023-12-11 13:44:45 +0100

    on ccommence

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-lfe <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 13:35:18 by jose-lfe          #+#    #+#             */
+/*   Updated: 2023/12/11 13:40:31 by jose-lfe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
