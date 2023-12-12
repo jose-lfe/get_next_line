2637e90af16a0e6e8cc6e8709f9ec299e34354c6
Jose Luis Fernandes Pereira <jose-lfe@c1r1s6.42lausanne.ch>
2023-12-12 14:08:07 +0100

    e

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,22 @@
 /*   By: jose-lfe <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:28:17 by jose-lfe          #+#    #+#             */
-/*   Updated: 2023/12/12 13:30:12 by jose-lfe         ###   ########.fr       */
+/*   Updated: 2023/12/12 14:02:18 by jose-lfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	find_line();
+
 char	*get_next_line(int fd)
 {
+	char	*line;
 	char	*buffer;
 
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	read(fd, );
+	if (!buffer)
+		return (NULL);
+	read(fd, buffer, BUFFER_SIZE);
+	return (line);
 }
