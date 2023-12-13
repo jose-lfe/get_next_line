feeb1338a63c2441f0fda5efdccfa9cb46499c87
Jose Luis Fernandes Pereira <jose-lfe@c1r3s4.42lausanne.ch>
2023-12-13 14:55:18 +0100

    12

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: jose-lfe <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:28:17 by jose-lfe          #+#    #+#             */
-/*   Updated: 2023/12/12 14:02:18 by jose-lfe         ###   ########.fr       */
+/*   Updated: 2023/12/13 14:51:17 by jose-lfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	find_line();
-
+static int	find_line(char *buffer, char *line);
+static int	ft_strlen(char *s);
 char	*get_next_line(int fd)
 {
 	char	*line;
@@ -22,6 +22,30 @@ char	*get_next_line(int fd)
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	read(fd, buffer, BUFFER_SIZE);
+	while (find_line(buffer, line) != 1)
+	{
+		read(fd, buffer, BUFFER_SIZE);
+		buffer[BUFFER_SIZE - 1] = '\0';
+	}
 	return (line);
+}
+
+static int ft_strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	find_line(char *buffer, char *line)
+{
+	int		i;
+	int		j;
+	char	*stock;
+
+	i = 0;
+	stock = malloc((ft_strlen(buffer)))
 }
